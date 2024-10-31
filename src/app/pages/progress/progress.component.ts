3243b76b1e3241b65996d5e6109ed598f40a000b
Gabriel Braga <gabribragandrade@gmail.com>
2024-10-31 10:37:58 -0300

    Progress page

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  daysOfMonth: { day: number, weekDay: string }[] = [];
+  weekDays: string[] = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'];
+  selectedDay: number = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+    const today = new Date();
+    const currentYear = today.getFullYear();
+    const currentMonth = today.getMonth() + 1;
+    this.generateDaysOfMonth(currentYear, currentMonth);
+    this.selectedDay = today.getDate();
+    console.log(this.daysOfMonth, this.selectedDay);
   }
 
+  generateDaysOfMonth(year: number, month: number): void {
+    const lastDay = new Date(year, month, 0).getDate();
+
+    for (let day = 1; day <= lastDay; day++) {
+      const currentDate = new Date(year, month - 1, day);
+      const weekDay = this.weekDays[currentDate.getDay()];
+      this.daysOfMonth.push({ day, weekDay });
+    }
+  }
 }
