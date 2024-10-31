e0b780b42728bbd4f54aa2dd227834b5f8e9429d
guigutox <guilhermegutofer@gmail.com>
2024-10-31 10:47:13 -0300

    adicionado service de cardapios

---
+++ src/app/services/cardapio/cardapio.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GlobalService } from '../global/global.service';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardapioService {
+
+
+  constructor(
+    private http: HttpClient,
+    private global: GlobalService,
+    private router: Router
+  ) { }
+
+  campusData(campus: string, data: string): Promise<boolean> {
+    return new Promise((resolve) => {
+      this.http.get(this.global.getApiUrl() + 'cardapio-dia/' + campus + "/" + data).subscribe(
+        (res: any) => {
+          console.log(res);
+          resolve(true);
+        },
+        err => {
+          console.log(err);
+          resolve(false);
+        }
+      );
+    });
+  }
+  
+  
+
+}
